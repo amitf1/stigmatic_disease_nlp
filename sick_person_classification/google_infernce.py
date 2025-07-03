65ecab02ce51bba9fd833cc72e99a1fb8e63816c
amitfe1 <amitfe1@gmail.com>
2025-07-03 17:59:22 +0300

    Created llm inference script for classification of the sick person in tweets.

---
+++ sick_person_classification/google_infernce.py
@@ -0,0 +1,90 @@
+# To run this code you need to install the following dependencies:
+# pip install google-genai && pip install pandas
+# or use the requirements.txt file
+
+import base64
+import json
+import os
+from google import genai
+from google.genai import types
+import pandas as pd
+import numpy as np
+
+def system_setup():
+    client = genai.Client(
+        api_key=os.environ.get("GEMINI_API_KEY"), ## create api key on google ai studio
+    )
+
+    model = "gemini-2.5-flash"
+
+    generate_content_config = types.GenerateContentConfig(
+    thinking_config = types.ThinkingConfig(
+        thinking_budget=0,
+    ),
+    response_mime_type="application/json",
+    response_schema=types.Schema(
+        type = types.Type.OBJECT,
+        properties = {
+            "real_sickness": types.Schema(
+                type = types.Type.BOOLEAN,
+            ),
+            "who_is_sick": types.Schema(
+                type = types.Type.STRING,
+            ),
+        },
+    ),
+    system_instruction=[
+        types.Part.from_text(text="""For each given tweet, analyse the sentence and the hashtags and decide if the author is speaking on a real sickness or just as a figuer of speach. 
+    If it is a real sickness, who is the sick person? Is it the author, friends or colleagues, family, a celebrity, other person or people or unclear. Answer with only one choice and only the choice itself. """),
+    ],
+    )
+    return client, model, generate_content_config
+
+def generate(input_text, client, model, generate_content_config):
+    
+    contents = [
+        types.Content(
+            role="user",
+            parts=[
+                types.Part.from_text(text=input_text),
+            ],
+        ),
+    ]
+
+    output = []
+    for chunk in client.models.generate_content_stream(
+        model=model,
+        contents=contents,
+        config=generate_content_config,
+    ):
+        output.append(chunk.text)
+    return json.loads("".join(output))
+
+def read_csv(file_path, tweet_column, limit=None, seed=42): 
+    df = pd.read_csv(file_path)
+    if limit:
+        df = df.sample(limit, random_state=seed)
+    return df[["tweet_id", tweet_column]]
+
+def inference(csv_path, tweet_column, output_path, limit=None):
+    results = []
+    client, model, generate_content_config = system_setup()
+    tweets = read_csv(csv_path, tweet_column, limit)
+    for index, row in tweets.iterrows():
+        result = generate(row[tweet_column], client, model, generate_content_config)
+        result["tweet_id"] = row["tweet_id"]
+        result["tweet"] = row[tweet_column]
+        results.append(result)
+    res_df = pd.DataFrame(results)
+    res_df.to_csv(output_path, index=False)
+
+
+if __name__ == "__main__":
+    csv_path = r"C:\Users\97254\Documents\all_tweets\new clusters\cancer_with_clusters.csv"
+    tweet_column = "Tweet_Text"
+    os.makedirs("outputs", exist_ok=True)
+    output_path = f"outputs/{os.path.basename(csv_path).split('.')[0]}_inferred_'_unclear.csv"
+    inference(csv_path, tweet_column, output_path, limit=10)
+
+
+
